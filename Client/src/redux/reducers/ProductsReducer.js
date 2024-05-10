ed37b969476393ca63fe347908357d43fbafc001
Akash1671998 <akash.vishwakarma@coraltele.com>
2024-05-10 18:02:24 +0530

    fix

--- Client/src/redux/reducers/ProductsReducer.js
+++ Client/src/redux/reducers/ProductsReducer.js
@@ -1,18 +1,15 @@
-
 import { GET_PRODUCTS_SUCCESS } from "../constaint/ProductConstaint";
 import { GET_PRODUCTS_FAILD } from "../constaint/ProductConstaint";
 
-export function getProductReducer (state,action){
+export function getProductReducer(state, action) {
 
-    switch(action.type){
-        case GET_PRODUCTS_SUCCESS,
-        return {products:action.payload}
-        case GET_PRODUCTS_FAILD;
-        return{error:action.payload}
-
+    switch (action.type) {
+        case GET_PRODUCTS_SUCCESS:
+            return { products: action.payload };
+        case GET_PRODUCTS_FAILD:
+            return { error: action.payload };
         default:
-            return state
-
+            return state;
     }
 
 }
